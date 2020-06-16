0d77fb2cee7942081b819fd905e3b331437f6583
dunghkt3k <66871088+dunghkt3k@users.noreply.github.com>
2020-06-16 19:46:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
 <?php
-echo 'test heroki lan thu n+1';
+
+
+
+class Student {
+
+    // gán giá trị mặc định cho thuộc tính $name
+    public $name = 'demo họ tên';
+
+
+    // phương thức tĩnh
+    public static function demo1() {
+        echo '<br>' . __METHOD__;
+        // không được sử dụng từ khóa $this trong phương thức tĩnh
+        // nếu sử dụng thì sẽ bị lỗi
+    }
+
+    // phương thức bình thường
+    public function demo2() {
+        echo '<br>' . __METHOD__;
+        echo '<br>' . $this->name;
+    }
+
+}
+
+// khởi tạo đối tượng từ class
+$tuan = new Student();
+// muốn gọi đến phương thức bình thường
+$tuan->demo2();
+
+// phương thức tĩnh có ưu điểm là có thể gọi đến phương thức tĩnh màn không
+// cần phải khởi tạo đối tượng
+// cách gọi TenClass::TenPhuongthucTinh
+Student::demo1();
+
   
 ?>
